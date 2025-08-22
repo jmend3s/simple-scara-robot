81a08951cd0f90e6f5877ce70a1279cd784ed97c
Jmendes42 <jmendes.3595@gmail.com>
2025-08-22 12:06:38 +0100

    gazebo lauch file

--- scara_ws/src/scara_description/launch/display.launch.py
+++ scara_ws/src/scara_description/launch/display.launch.py
@@ -1,12 +1,15 @@
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, LaunchConfiguration
+
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
-from launch.actions import DeclareLaunchArgument
+
 from ament_index_python.packages import get_package_share_directory
 
 import os
+
 
 def generate_launch_description():
     model_arg = DeclareLaunchArgument(
