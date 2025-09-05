b829b454733160dcae61729391b436d5768e7c01
Jmendes42 <jmendes.3595@gmail.com>
2025-09-05 16:26:48 +0100

    MoveIt configuration files done not tested

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-# Source ROS workspace if it exists
 if [ -f /scara_ws/install/setup.bash ]; then
     source /scara_ws/install/setup.bash
 fi
 
-# Keep container alive with bash
 exec bash
+echo "export RMW_IMPLEMENTATION=rmw-cyclonedds-cpp" >> /home/void/.bashrc
+
