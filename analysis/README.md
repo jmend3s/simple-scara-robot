6a40824834bc97db0e0f5146bd6d8fa90fc34fd8
Jmendes42 <jmendes.3595@gmail.com>
2025-09-30 19:11:35 +0100

    Displacement vectors

---
+++ analysis/README.md
@@ -0,0 +1,13 @@
+# Robot Analysis
+
+## Rotation Matrices
+
+Here is the analysis of the rotation matrices between all the joints from the base to end effector.<br>
+![Analysis](../assets/images/rotation_matrices.jpg)<br>
+
+
+## Displacement Vectors
+
+![Analysis](../assets/images/displacement_vectors.jpg)<br>
+
+The file ```analysis.py``` confirms the calculations on the image and will serve as a way to confirm the truth on the robot movements
--- analysis/analysis.py
+++ analysis/analysis.py
@@ -13,4 +13,15 @@ rotationMatrix1_2 = [[np.cos(theta2), -np.sin(theta2), 0], [np.sin(theta2), np.c
 
 rotationMatrix0_2 = np.dot(rotationMatrix1_2, rotationMatrix0_1)
 
-print(rotationMatrix0_2)
+# print(rotationMatrix0_2)
+
+a2 = 59.75
+a3 = 56
+a4 = 46.975
+a5 = 41.738
+
+displacementVector0_1 = [[a2*np.cos(theta1)], [a2*np.sin(theta1)], [a3]]
+displacementVector1_2 = [[a4*np.cos(theta2)], [a4*np.sin(theta2)], [-a5]]
+
+print(np.matrix(displacementVector0_1))
+print(np.matrix(displacementVector1_2))
