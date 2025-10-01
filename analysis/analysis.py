469683912573e1769a0f0e6310c33f0c27503870
Jmendes42 <jmendes.3595@gmail.com>
2025-10-01 03:48:41 +0100

    Homogeneous transformation matrix on python

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -23,5 +23,14 @@ a5 = 41.738
 displacementVector0_1 = [[a2*np.cos(theta1)], [a2*np.sin(theta1)], [a3]]
 displacementVector1_2 = [[a4*np.cos(theta2)], [a4*np.sin(theta2)], [-a5]]
 
-print(np.matrix(displacementVector0_1))
-print(np.matrix(displacementVector1_2))
+# print(np.matrix(displacementVector0_1))
+
+homogeneousMatrix0_1 = np.concatenate((rotationMatrix0_1, displacementVector0_1), axis = 1)
+homogeneousMatrix0_1 = np.concatenate((homogeneousMatrix0_1, [[0, 0, 0, 1]]), axis = 0)
+
+homogeneousMatrix1_2 = np.concatenate((rotationMatrix1_2, displacementVector1_2), axis = 1)
+homogeneousMatrix1_2 = np.concatenate((homogeneousMatrix1_2, [[0, 0, 0, 1]]), axis = 0)
+
+homogeneousMatrix0_2 = np.dot(homogeneousMatrix0_1, homogeneousMatrix1_2)
+
+print(np.matrix(homogeneousMatrix0_2))
