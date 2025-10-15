07e1260d489f1f560175bd04f180a695a68fc298
Jmendes42 <jmendes.3595@gmail.com>
2025-10-15 10:53:43 +0100

    Inverse kinematics aparently ok

--- src/main.cpp
+++ src/main.cpp
@@ -33,14 +33,16 @@ void setServoAngle(int channel, int angle)
 
 void loop()
 {
-    float x = 5.0f;
-    float y = 6.0f;
+    float x = 3.0f;
+    float y = 5.0f;
 
     float xSqr = x * x;
     float ySqr = y * y;
 
     float L1 = 6.0f;
     float L2 = 4.7f;
+    float L3 = 0.1f;
+    float L4 = 2.8f;
 
     float L1Sqr = L1 * L1;
     float L2Sqr = L2 * L2;
@@ -49,10 +51,13 @@ void loop()
     float beta = acos(preBeta);
     float preAlpha = (xSqr + ySqr + L1Sqr - L2Sqr) / (2 * L1 * sqrt(xSqr + ySqr));
     float alpha = acos(preAlpha);
-    float phi = atan2(y, x);                                                                                                                         
+    float phi = atan2(y, x);
+
+    float endEffectorAngle = atan2(L4, L2 + L3);
 
     float theta1 = phi - alpha;
     float theta2 = M_PI - beta;
+    theta2 = theta2 - endEffectorAngle;
 
     int pwm1 = constrain(round(theta1 / M_PI * 180.0), 0, 180);
     int pwm2 = constrain(round(theta2 / M_PI * 180.0), 0, 180);
